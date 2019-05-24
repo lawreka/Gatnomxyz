d45fc3fb29cbb60fa61b29e78ec7be9ba241d9fd
Kathryn Lawrence <klaw@MacBook-Pro.local>
2019-05-24 20:41:32 +0200

    Initial commit

---
+++ assets/js/gatnom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#issuesmenulink").click(function(){
+    $("#issuesmenu").toggleClass("hidden");
+  });
+  $("#hamburger-menu").click(function(){
+    $("#menu-container").toggleClass("hidden");
+  });
+  $("#antihamburger").click(function(){
+    $("#menu-container").toggleClass("hidden");
+  });
+});
+
+// print issues cover title reveal
+// $(".MONTAGcover").on("hover", function(){
+//
+// });
+
+ // menu functionality
+
+    //
+    // return false;
