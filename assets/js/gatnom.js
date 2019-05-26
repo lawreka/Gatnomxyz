0870e7c658b5a72d3311204030d5fd72eb0a6473
Kathryn Lawrence <klaw@MacBook-Pro.fritz.box>
2019-05-26 12:46:07 +0200

    updated typography, removed desktop logobar, added js for typing subheader

--- assets/js/gatnom.js
+++ assets/js/gatnom.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
-  $("#openmodal").click(function(){
+
+  document.getElementById("openmodal").addEventListener("click", openModal);
+
+  document.getElementById("closemodal").addEventListener("click", closeModal);
+
+  var i = 0;
+  var txt = 'A magazine that writes itself.'; /* The text */
+  var speed = 50;
+  function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("subheader").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+  }
+
+  function clearText(){
+    document.getElementById("subheader").innerHTML = "";
+    i = 0;
+  }
+
+  function openModal(){
+    console.log("open!");
     event.preventDefault();
     $("#modal").toggleClass("hidden");
-  });
-  $("#closemodal").click(function(){
+    typeWriter();
+  }
+
+  function closeModal(){
+    console.log("close!");
     event.preventDefault();
     $("#modal").toggleClass("hidden");
-  });
+    clearText();
+  }
+
 });
